d026994772cd983d4e08b523b542ea5b25e4f514
Shaeikh <doyouhere4@gmail.com>
2026-08-16 16:16:08 +0500

    Add meta data config

---
+++ config/metadata.ts
@@ -0,0 +1,14 @@
+export const pageMetadata = {
+  login: {
+    title: "Login",
+    description: "Access your account details.",
+  },
+  signup: {
+    title: "Sign Up",
+    description: "Create an account on our platform.",
+  },
+  chat: {
+    title: "Chat Workspace",
+    description: "Connect with your team.",
+  },
+};
