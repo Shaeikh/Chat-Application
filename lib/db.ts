f0ea55cbbeef8f5eb49e701acb010f371a58ff82
Shaeikh <doyouhere4@gmail.com>
2026-08-22 15:31:35 +0500

    Move from bun:sqlite to better-sqlite3

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { Database } from "bun:sqlite";
+import Database from "better-sqlite3";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
@@ -6,9 +6,9 @@ const dbPath = path.join(process.cwd(), "database.sqlite");
 
 const db = new Database(dbPath);
 
-db.run("PRAGMA journal_mode = WAL");
+db.pragma("journal_mode = WAL");
 
-db.run(
+db.exec(
   `
 CREATE TABLE IF NOT EXISTS messages (
     id TEXT PRIMARY KEY,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS messages (
     type TEXT NOT NULL CHECK(type IN ('normal', 'system')),
     content TEXT NOT NULL,
     created_at INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id) 
+    FOREIGN KEY (user_id) REFERENCES "user"(id)
 );
 CREATE INDEX IF NOT EXISTS messages_room_id_idx
     ON messages(room, id)
